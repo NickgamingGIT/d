3d246ebc52282a488cb4d643b75d1f7614f5da82
Nicholas Thompson <nickgaming005@hotmail.com>
2025-07-06 22:08:02 -0700

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits } from "discord.js";
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login("1391606382800932915");
